b683650b0a8011db5c1a98dc648ec6089ec0663f
SadHamburger1037 <marioheadphones6@gmail.com>
2023-12-12 11:07:07 +0100

    registracija

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Moja prva aplikacija' });
 });
+
+router.get('/test', function(req, res, next) {
+  res.render('test');
+});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Joi = require('joi');
+var fs = require('fs');
 
 router.get('/signin', function (req, res, next) {
   res.render('users/signin', { status: { display_form: true, error: false } });
@@ -11,7 +12,7 @@ const schema_signin = Joi.object({
   password: Joi.string().required().max(50)
 });
 
-router.post('/signin', function (req, res, next) {
+router.post('/signin', async function (req, res, next) {
 
   const data = req.body
 
@@ -21,7 +22,37 @@ router.post('/signin', function (req, res, next) {
     res.render('users/signin', { status: { submit_form: true, error: true } });
   } else {
     // TODO: prijava korisnika
+    fs.writeFileSync('data.JSON', JSON.stringify(data))
     res.render('users/signin', { status: { submit_form: true, error: false } });
+
+    /*await new Promise(resolve => {
+      setTimeout(resolve, 5000);
+    });
+    res.redirect('test');*/
+  }
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('users/register', { status: { display_form: true, error: false } });
+});
+
+router.post('/register', async function (req, res, next) {
+
+  const data = req.body
+
+  const validation = schema_signin.validate(data);
+
+  if (validation.error) {
+    res.render('users/register', { status: { submit_form: true, error: true } });
+  } else {
+    // TODO: prijava korisnika
+    fs.writeFileSync('data.JSON', JSON.stringify(data))
+    res.render('users/register', { status: { submit_form: true, error: false } });
+
+    /*await new Promise(resolve => {
+      setTimeout(resolve, 5000);
+    });
+    res.redirect('test');*/
   }
 });
 

87f7879f1520dc9565a49787ea885c6728982134
SadHamburger1037 <marioheadphones6@gmail.com>
2023-12-12 10:06:56 +0100

    Prijava korisnika

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var Joi = require('joi');
 
-router.get('/signin', function(req, res, next) {
-  res.render('users/signin', { title: 'Prijava korisnika' });
+router.get('/signin', function (req, res, next) {
+  res.render('users/signin', { status: { display_form: true, error: false } });
+});
+
+const schema_signin = Joi.object({
+  email: Joi.string().email().max(50).required(),
+  password: Joi.string().required().max(50)
+});
+
+router.post('/signin', function (req, res, next) {
+
+  const data = req.body
+
+  const validation = schema_signin.validate(data);
+
+  if (validation.error) {
+    res.render('users/signin', { status: { submit_form: true, error: true } });
+  } else {
+    // TODO: prijava korisnika
+    res.render('users/signin', { status: { submit_form: true, error: false } });
+  }
 });
 
 module.exports = router;

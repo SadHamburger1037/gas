03b694d3bc91d35bd0f65741bc0e5d1a739d25dc
SadHamburger1037 <marioheadphones6@gmail.com>
2023-12-13 12:45:46 +0100

    lol

--- routes/users.js
+++ routes/users.js
@@ -18,17 +18,19 @@ router.post('/signin', async function (req, res, next) {
 
   const validation = schema_signin.validate(data);
 
+  /*var podatci = require('../data.JSON')
+
+  for (let i = 0; i < podatci.length; i++){
+    if (data = podatci[i]){
+      res.redirect('test');
+    }
+  }*/
+
   if (validation.error) {
     res.render('users/signin', { status: { submit_form: true, error: true } });
   } else {
     // TODO: prijava korisnika
-    fs.writeFileSync('data.JSON', JSON.stringify(data))
     res.render('users/signin', { status: { submit_form: true, error: false } });
-
-    /*await new Promise(resolve => {
-      setTimeout(resolve, 5000);
-    });
-    res.redirect('test');*/
   }
 });
 
@@ -36,23 +38,19 @@ router.get('/register', function (req, res, next) {
   res.render('users/register', { status: { display_form: true, error: false } });
 });
 
-router.post('/register', async function (req, res, next) {
+router.post('/register', function (req, res, next) {
 
   const data = req.body
 
   const validation = schema_signin.validate(data);
-
   if (validation.error) {
     res.render('users/register', { status: { submit_form: true, error: true } });
   } else {
     // TODO: prijava korisnika
     fs.writeFileSync('data.JSON', JSON.stringify(data))
+    console.log(data)
     res.render('users/register', { status: { submit_form: true, error: false } });
 
-    /*await new Promise(resolve => {
-      setTimeout(resolve, 5000);
-    });
-    res.redirect('test');*/
   }
 });
 
